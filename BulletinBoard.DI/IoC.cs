88ab1cf1c6e0cf7978f829147c8cacf5afb694ae
EugeneTryfanau <eugenetryfanau@gmail.com>
2023-09-07 14:26:18 +0300

    Добавил пока структуру и подключил пару пакетов (Entity и DependencyInjection)

---
+++ BulletinBoard.DI/IoC.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulletinBoard.DI
+{
+    static internal class IoC
+    {
+        static public void IoCCommonRegister()
+        {
+
+        }
+    }
+}
