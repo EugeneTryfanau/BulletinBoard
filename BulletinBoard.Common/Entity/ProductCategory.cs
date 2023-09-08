cdbdde10f93f04f9c5685d55b88ae2d9beb864ef
EugeneTryfanau <eugenetryfanau@gmail.com>
2023-09-08 17:34:05 +0300

    Основные классы. Остальные пока не добавлял, думаю как их связать и реализовать эту связь

---
+++ BulletinBoard.Common/Entity/ProductCategory.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulletinBoard.Common.Entity
+{
+    public class ProductCategory
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string CategoryName { get; set; }
+
+        public int ParentId { get; set; }
+    }
+}
