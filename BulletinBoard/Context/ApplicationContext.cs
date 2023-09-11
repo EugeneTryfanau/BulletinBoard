14ffa4e08e703468af01ddd6334cff4fa33131b0
EugeneTryfanau <eugenetryfanau@gmail.com>
2023-09-11 15:47:51 +0300

    Контекст для создания и подключения к бд (макет), скорее всего придется переделывать (перенести в DAL, а здесь отсавить только создание, посмотрим)

---
+++ BulletinBoard/Context/ApplicationContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BulletinBoard.Context
+{
+    public class ApplicationContext: DbContext
+    {
+        //напоминание: стандартная строка для объявления сущностей в бд для entity
+        //public DbSet<> Name => Set<>();
+
+        public ApplicationContext() => Database.EnsureCreated();
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=BulletinBoardDB.db");
+        }
+    }
+}
