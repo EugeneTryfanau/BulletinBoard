37726fdb8801fd5c419d5758a5546903c4f4b79a
EugeneTryfanau <eugenetryfanau@gmail.com>
2023-09-12 12:27:13 +0300

    Проблемы с DAL, библиотека не core и нет возможности подключить entity core, решил перенести слой DAL на консольку

--- BulletinBoard/Context/ApplicationContext.cs
+++ BulletinBoard/Context/ApplicationContext.cs
@@ -1,11 +1,21 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using BulletinBoard.Common.Entity;
+using BulletinBoard.Common.Models.AuthorisationModels;
+using Microsoft.EntityFrameworkCore;
 
 namespace BulletinBoard.Context
 {
     public class ApplicationContext: DbContext
     {
         //напоминание: стандартная строка для объявления сущностей в бд для entity
-        //public DbSet<> Name => Set<>();
+        //public DbSet<class> Name => Set<class>();
+
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+
+        public DbSet<UserModel> UserModels { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Picture> Picture { get; set; }
 
         public ApplicationContext() => Database.EnsureCreated();
 
