69799f2b1764ed2122a829fb8c4650c5bbe4b330
EugeneTryfanau <eugenetryfanau@gmail.com>
2023-10-23 18:53:31 +0300

    Начальный эндпоинт для загрузки фото

---
+++ BulletinBoard/Endpoints/PhotoEndpoints.cs
@@ -0,0 +1,15 @@
+﻿using BulletinBoard.DAL.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulletinBoard.Endpoints
+{
+    public class PhotoEndpoints
+    {
+        public static async Task<IResult> AddPhoto(ApplicationDbContext db, IFormFile file, int productId)
+        {
+            var product = await db.Products.FirstOrDefaultAsync(x => x.Id == productId);
+
+            return Results.Ok();
+        }
+    }
+}
