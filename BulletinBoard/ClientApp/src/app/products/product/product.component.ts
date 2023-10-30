ece2f59c0ad279487e874c19f67b4a58f362f672
EugeneTryfanau <eugenetryfanau@gmail.com>
2023-10-30 19:41:46 +0300

    добавил отображение изображений в объявлении и их перелистывание

--- BulletinBoard/ClientApp/src/app/products/product/product.component.ts
+++ BulletinBoard/ClientApp/src/app/products/product/product.component.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, SimpleChanges } from '@angular/core';
 import { ProductService } from '../../services/product.service';
 import { ActivatedRoute } from '@angular/router';
-import { switchMap } from 'rxjs/operators';
+import { delay, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-product',
@@ -11,20 +11,55 @@ import { switchMap } from 'rxjs/operators';
 export class ProductComponent {
 
   productId!: number;
-  private sub: any;
   product: any;
+  pictures: any;
+  picIndexes: number[] = [];
+
+  slideIndex: number = 1;
+  slideChanged: boolean = false;
 
   constructor(private route: ActivatedRoute, private prod: ProductService) {
   }
 
   async ngOnInit() {
 
-    this.route.paramMap.pipe(
+    await this.route.paramMap.pipe(
       switchMap(params => params.getAll('productId')))
       .subscribe(data => this.productId = +data);
 
     this.product = await this.prod.getProductInfo(this.productId);
-    console.log(this.product);
+    this.pictures = this.product.pictures;
+    for (let i = 0; i < this.pictures.length; i++) {
+      this.picIndexes.push(i + 1);
+    }
   }
 
+  plusSlides(n: number) {
+    this.showSlides(this.slideIndex += n);
+  }
+
+  currentSlide(n: number) {
+    this.showSlides(this.slideIndex = n);
+  }
+
+  async showSlides(n: number) {
+    var i;
+    var slides = await Array.from(await document.getElementsByClassName('mySlides') as HTMLCollectionOf<HTMLElement>);
+    console.log(slides);
+
+    var dots = await Array.from(document.getElementsByClassName('dot') as HTMLCollectionOf<HTMLElement>);
+    console.log(dots);
+    if (n > slides.length) { this.slideIndex = 1 }
+    if (n < 1) { this.slideIndex = slides.length }
+      for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";
+      }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[this.slideIndex - 1].style.display = "block";
+    dots[this.slideIndex - 1].className += " active";
+    this.slideChanged = true;
+  }
+  
 }
