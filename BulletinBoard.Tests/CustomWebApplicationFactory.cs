3c39b902689998858d425750a44fab977ce432f4
EugeneTryfanau <eugenetryfanau@gmail.com>
2023-11-10 12:35:48 +0300

    Конфигурация WebApplicationFactory чтобы получать доступ к базе и EF не закрыл подключение

---
+++ BulletinBoard.Tests/CustomWebApplicationFactory.cs
@@ -0,0 +1,45 @@
+﻿using BulletinBoard.DAL.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System.Data.Common;
+
+namespace BulletinBoard.Tests
+{
+    public class CustomWebApplicationFactory<TProgram> : WebApplicationFactory<TProgram> where TProgram : class
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                var dbContextDescriptor = services.SingleOrDefault(
+                    b => b.ServiceType == typeof(DbContextOptions<ApplicationDbContext>));
+
+                services.Remove(dbContextDescriptor);
+
+                var dbConnectionDescriptor = services.SingleOrDefault(
+                    b => b.ServiceType == typeof(DbConnection));
+
+                services.Remove(dbConnectionDescriptor);
+
+                services.AddSingleton<DbConnection>(conteiner =>
+                {
+                    var connection = new SqlConnection("Server=(localdb)\\mssqllocaldb;Database=BulletinBoard;Trusted_Connection=True;MultipleActiveResultSets=true");
+                    connection.Open();
+
+                    return connection;
+                });
+
+                services.AddDbContext<ApplicationDbContext>((conteiner, options) =>
+                {
+                    var connection = conteiner.GetRequiredService<DbConnection>();
+                    options.UseSqlServer(connection);
+                });
+            });
+
+            builder.UseEnvironment("Development");
+        }
+    }
+}
